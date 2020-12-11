4e21aaac673680e703d2df443bf8f5c957fbef6b
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2020-12-11 21:45:10 +0700

    Applying async await to function

--- js/script.js
+++ js/script.js
@@ -1,60 +1,27 @@
-// let quotes = [
-//     {
-//         id: "1",
-//         text: `"This is the Book about which there is no doubt, a guidance for those conscious of Allah"`,
-//         author: "God in the Quran 2 : 2"
-//     },
-//     {
-//         id: "2",
-//         text: `"Every soul will taste death, and you will only be given your [full] compensation on the Day of Resurrection"`,
-//         author: "God in the Quran 3 : 185"
-//     }
-// ];
-    
-// let getNewIndex = () => Math.floor(Math.random() * quotes.length);
-
-// window.sessionStorage;
-// sessionStorage.setItem("currentIndex", getNewIndex.toString());
-
-// let getQuoteIndex = () => {
-//     let newIndex = getNewIndex();
-
-//     while(newIndex == sessionStorage.getItem("currentIndex")) {
-//         newIndex = getNewIndex();
-//     }
-//     sessionStorage.setItem("currentIndex", newIndex.toString());
-//     return newIndex;
-// };
-
-// let index = getQuoteIndex();
-
 async function getNewVerse() {
-    let newVerse;
+    let response = await fetch("https://5f9b77e3856f4c00168bffb3.mockapi.io/quran-verse");
+    let data = await response.json();
 
-    fetch("https://5f9b77e3856f4c00168bffb3.mockapi.io/quran-verse")
-    .then(response => response.json())
-    .then(function(data) {
-        newVerse = Object.assign({}, data[Math.floor(Math.random() * data.length)]);
-    })
-    .catch(function(err) {
-        console.log("Fetch error:", err)
-    })
-
-    return newVerse;
+    // console.log(data);
+    return data;
 }
+
+getNewVerse();
+// let verse = getNewVerse().then(value => value);
+// console.log(verse);
 
 let tweetIntentURL = "https://twitter.com/intent/tweet?hashtags=Quran&related=freecodecamp&text=";
 
-$(document).ready(function () {
-    const verse = await getNewVerse();
-    $("#text").text(verse.text);
-    $("#author").text("- " + verse.author);
-    $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
+// $(document).ready(function () {
+//     const verse = Object.assign({}, getNewVerse().then());
+//     $("#text").text(verse.text);
+//     $("#author").text("- " + verse.author);
+//     $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
 
-    $("#new-quote").on("click", function () {
-        const verse = await getNewVerse();
-        $("#text").text(verse.text);
-        $("#author").text("- " + verse.author);
-        $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
-    });
-});
+//     $("#new-quote").on("click", function () {
+//         const verse = Object.assign({}, getNewVerse());
+//         $("#text").text(verse.text);
+//         $("#author").text("- " + verse.author);
+//         $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
+//     });
+// });
