db97054c040b974f0afb490627741a7c5c865477
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2020-12-11 16:34:37 +0700

    Getting mockapi to works

--- js/script.js
+++ js/script.js
@@ -1,40 +1,60 @@
-let quotes = [
-    {
-        text: `"This is the Book about which there is no doubt, a guidance for those conscious of Allah"`,
-        author: "God in the Quran 2 : 2"
-    },
-    {
-        text: `"Every soul will taste death, and you will only be given your [full] compensation on the Day of Resurrection"`,
-        author: "God in the Quran 3 : 185"
-    }
-];
+// let quotes = [
+//     {
+//         id: "1",
+//         text: `"This is the Book about which there is no doubt, a guidance for those conscious of Allah"`,
+//         author: "God in the Quran 2 : 2"
+//     },
+//     {
+//         id: "2",
+//         text: `"Every soul will taste death, and you will only be given your [full] compensation on the Day of Resurrection"`,
+//         author: "God in the Quran 3 : 185"
+//     }
+// ];
+    
+// let getNewIndex = () => Math.floor(Math.random() * quotes.length);
 
-let getNewIndex = () => Math.floor(Math.random() * quotes.length);
+// window.sessionStorage;
+// sessionStorage.setItem("currentIndex", getNewIndex.toString());
 
-window.sessionStorage;
-sessionStorage.setItem("currentIndex", getNewIndex.toString());
+// let getQuoteIndex = () => {
+//     let newIndex = getNewIndex();
 
-let getQuoteIndex = () => {
-    let newIndex = getNewIndex();
+//     while(newIndex == sessionStorage.getItem("currentIndex")) {
+//         newIndex = getNewIndex();
+//     }
+//     sessionStorage.setItem("currentIndex", newIndex.toString());
+//     return newIndex;
+// };
 
-    while(newIndex == sessionStorage.getItem("currentIndex")) {
-        newIndex = getNewIndex();
-    }
-    sessionStorage.setItem("currentIndex", newIndex.toString());
-    return newIndex;
-};
+// let index = getQuoteIndex();
 
-let index = getQuoteIndex();
+async function getNewVerse() {
+    let newVerse;
+
+    fetch("https://5f9b77e3856f4c00168bffb3.mockapi.io/quran-verse")
+    .then(response => response.json())
+    .then(function(data) {
+        newVerse = Object.assign({}, data[Math.floor(Math.random() * data.length)]);
+    })
+    .catch(function(err) {
+        console.log("Fetch error:", err)
+    })
+
+    return newVerse;
+}
+
 let tweetIntentURL = "https://twitter.com/intent/tweet?hashtags=Quran&related=freecodecamp&text=";
 
 $(document).ready(function () {
-    $("#text").text(quotes[index].text);
-    $("#author").text("- " + quotes[index].author);
+    const verse = await getNewVerse();
+    $("#text").text(verse.text);
+    $("#author").text("- " + verse.author);
+    $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
 
     $("#new-quote").on("click", function () {
-        index = getQuoteIndex();
-        $("#text").text(quotes[index].text);
-        $("#author").text("- " + quotes[index].author);
-        $("#tweet-quote").attr("href", tweetIntentURL+(quotes[index].text)+" - "+(quotes[index].author));
+        const verse = await getNewVerse();
+        $("#text").text(verse.text);
+        $("#author").text("- " + verse.author);
+        $("#tweet-quote").attr("href", tweetIntentURL+(verse.text)+" - "+(verse.author));
     });
 });
