253173058de467fec985060426e9cdae9d27f62e
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-02-09 23:00:08 +0700

    Added more verses

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 let quotes = [
     {
+        id: "1",
         text: '"This is the Book about which there is no doubt, a guidance for those conscious of Allah"',
         author: "God in the Quran 2 : 2"
     },
     {
+        id: "2",
         text: '"Every soul will taste death, and you will only be given your [full] compensation on the Day of Resurrection"',
         author: "God in the Quran 3 : 185"
+    },
+    {
+        id: "3",
+        text: "Or were they created by nothing, or were they the creators [of themselves]?",
+        author: "God in the Quran 52 : 35"
+    },
+    {
+        id: "4",
+        text: "Do they not then reflect on the Quran? Had it been from anyone other than Allah, they would have certainly found in it many inconsistencies",
+        author: "God in the Quran 4 : 82"
+    },
+    {
+        id: "5",
+        text: "And He is the One Who originates the creation then will resurrect it—which is even easier for Him. To Him belong the finest attributes in the heavens and the earth. And He is the Almighty, All-Wise",
+        author: "God in the Quran 30 : 27"
     }
 ];
 
